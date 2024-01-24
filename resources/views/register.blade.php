32bb38b71156c50dd1aef16822d1fb0bf6cf6ac6
Dhaivath <dhaivathl@gmail.com>
2024-01-24 23:38:15 +0530

    lite changes

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -8,7 +8,7 @@
             @csrf
             <div class="m-3">
                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email</label>
-                <input name="email" type="text" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Enter your email" required>
+                <input name="email" type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Enter your email" required>
             </div>
 
             <input type="submit" value="Send Registration Link" class="m-3 text-white my-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 focus:outline-none">
