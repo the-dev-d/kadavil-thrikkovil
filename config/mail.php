c042b89475bef67e9657bf84ce12319535dfa916
the-dev-d <dhaivathl@gmail.com>
2024-02-06 22:30:56 +0530

    mail config change

--- config/mail.php
+++ config/mail.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'smtp'),
+    'default' => env('MAIL_MAILER', 'smtp'),    
 
     /*
     |--------------------------------------------------------------------------
@@ -32,6 +32,13 @@ return [
     |            "postmark", "log", "array", "failover", "roundrobin"
     |
     */
+    'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+        ],
+    ]
 
     'mailers' => [
         'smtp' => [
